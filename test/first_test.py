2be82227ce35ec7a7c17a7fe700cfd4c21c80ab0
Polina-PC\Polina <funky.mandarin@gmail.com>
2017-11-01 19:11:32 +0300

    Файл с фикстурой перенесён в корневой каталог

--- test/first_test.py
+++ test/first_test.py
@@ -1,6 +1,6 @@
 def test_create_group(app):
-    app.group.create_group(username="admin", password="secret", name="Test group", header="Header 4 test group2",
-                             footer="Footer 4 test group2")
+    app.group.create_group(username="admin", password="secret", name="Test group", header="Header 4 test group",
+                             footer="Footer 4 test group")
 
 
 def test_create_empty_group(app):
