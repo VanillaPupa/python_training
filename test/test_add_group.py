edaaf7e258bc0732431bfbc0bf5670a5e90252ea
Polina <funky.mandarin@gmail.com>
2017-11-06 05:18:31 +0300

    Изменения для 8 задания. Ошибка в методе session.is_logged_in

--- test/test_add_group.py
+++ test/test_add_group.py
@@ -5,6 +5,7 @@ def test_create_group(app):
     group_form = Group(name="Test group", header="Header 4 group", footer="Footer 4 group")
     app.group.create(group_form)
     # app.group.create_group(app, group_form, username="admin", password="secret")
+    #app.session.ensure_logout()
 
 
 def test_create_empty_group(app):
