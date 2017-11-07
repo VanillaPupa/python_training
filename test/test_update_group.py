2ae859a01714178f6dcd0d4f41c8aa3653a2f53f
Polina <funky.mandarin@gmail.com>
2017-11-07 03:14:44 +0300

    В тест с редактированием группы добавлена проверка наличия групп в списке. Если групп нет, будет создана тестовая группа.

--- test/test_update_group.py
+++ test/test_update_group.py
@@ -2,15 +2,24 @@ from model.group import Group
 
 
 def test_update_first_group(app):
+    if app.group.count() == 0:
+        group_form = Group(name="Test group")
+        app.group.create(group_form)
     group_form = Group(name="Updated group", header="Updated Header 4 group", footer="Updated Footer 4 group")
     app.group.update_first(group_form)
 
 
 def test_update_name_first_group(app):
+    if app.group.count() == 0:
+        group_form = Group(name="Test group")
+        app.group.create(group_form)
     group_form = Group(name="Updated Name")
     app.group.update_first(group_form)
 
 
 def test_update_header_first_group(app):
+    if app.group.count() == 0:
+        group_form = Group(name="Test group")
+        app.group.create(group_form)
     group_form = Group(header="Updated Header")
     app.group.update_first(group_form)
