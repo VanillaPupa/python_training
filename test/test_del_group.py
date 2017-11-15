d690c2eada17ff4ff559f0dcaf5f5c42da5e3a32
Polina <vanillapupa@gmail.com>
2017-11-16 02:31:20 +0300

    В тест по удалению групы добавлена проверка-сравнение количества групп

--- test/test_del_group.py
+++ test/test_del_group.py
@@ -5,4 +5,7 @@ def test_del_first_group(app):
     if app.group.count() == 0:
         group_form = Group(name="Test group")
         app.group.create(group_form)
+    old_groups = app.group.get_group_list()
     app.group.delete_first()
+    new_groups = app.group.get_group_list()
+    assert len(old_groups) - 1 == len(new_groups)
