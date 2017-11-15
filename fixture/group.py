5db13c8a655b63f9a1e3cffcb996e573aca42aa2
Polina <vanillapupa@gmail.com>
2017-11-16 01:38:52 +0300

    Добавлен метод, возвращающий список групп. В тесты по созданию групп добавлены проверки-сравнения количества групп

--- fixture/group.py
+++ fixture/group.py
@@ -1,3 +1,5 @@
+from model.group import Group
+
 class GroupHelper:
 
     def __init__(self, app):
@@ -64,6 +66,16 @@ class GroupHelper:
         wd = self.app.wd
         self.open_groups_page()
         return len(wd.find_elements_by_name("selected[]"))
+
+    def get_group_list(self):
+        wd = self.app.wd
+        self.open_groups_page()
+        group_list = []
+        for element in wd.find_elements_by_css_selector("span.group"):
+            text = element.text
+            id = element.find_element_by_name("selected[]").get_attribute("value")
+            group_list.append(Group(name=text, id=id))
+        return group_list
 
     # def create_group(self, app, group_form, username, password):
         # app.session.login(username, password)
--- test/test_add_group.py
+++ test/test_add_group.py
@@ -2,10 +2,16 @@ from model.group import Group
 
 
 def test_create_group(app):
+    old_groups = app.group.get_group_list()
     group_form = Group(name="Test group", header="Header 4 group", footer="Footer 4 group")
     app.group.create(group_form)
+    new_groups = app.group.get_group_list()
+    assert len(old_groups) + 1 == len(new_groups)
 
 
 def test_create_empty_group(app):
+    old_groups = app.group.get_group_list()
     group_form = Group(name="", header="", footer="")
     app.group.create(group_form)
+    new_groups = app.group.get_group_list()
+    assert len(old_groups) + 1 == len(new_groups)
