ccbb0b7ab822cf6cac1a4ea23d34d212d8e6da21
Polina <vanillapupa@gmail.com>
2017-11-16 03:05:14 +0300

    В тесты по редактированию групп добавлены проверки-сравнения количества групп

--- test/test_update_group.py
+++ test/test_update_group.py
@@ -5,21 +5,30 @@ def test_update_first_group(app):
     if app.group.count() == 0:
         group_form = Group(name="Test group")
         app.group.create(group_form)
+    old_groups = app.group.get_group_list()
     group_form = Group(name="Updated group", header="Updated Header 4 group", footer="Updated Footer 4 group")
     app.group.update_first(group_form)
+    new_groups = app.group.get_group_list()
+    assert len(old_groups) == len(new_groups)
 
 
 def test_update_name_first_group(app):
     if app.group.count() == 0:
         group_form = Group(name="Test group")
         app.group.create(group_form)
+    old_groups = app.group.get_group_list()
     group_form = Group(name="Updated Name")
     app.group.update_first(group_form)
+    new_groups = app.group.get_group_list()
+    assert len(old_groups) == len(new_groups)
 
 
 def test_update_header_first_group(app):
     if app.group.count() == 0:
         group_form = Group(name="Test group")
         app.group.create(group_form)
+    old_groups = app.group.get_group_list()
     group_form = Group(header="Updated Header")
     app.group.update_first(group_form)
+    new_groups = app.group.get_group_list()
+    assert len(old_groups) == len(new_groups)
