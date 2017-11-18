ca50b02a5b2bda829681b48f6e8aa1408d3c2b53
Polina <vanillapupa@gmail.com>
2017-11-18 04:11:24 +0300

    Добавлена проверка списков в тест для удаления контакта

--- test/test_del_user.py
+++ test/test_del_user.py
@@ -8,4 +8,9 @@ def test_del_first_user(app):
                        email2="Holmes@Museum.com", hometel="3213213", mobiletel="+441712223355")
         app.user.add(contact)
         app.user.open_home_page()
+    old_user_list = app.user.get_user_list()
     app.user.delete_first()
+    new_user_list = app.user.get_user_list()
+    assert len(old_user_list) - 1 == len(new_user_list)
+    old_user_list[0:1] = []
+    assert old_user_list == new_user_list
