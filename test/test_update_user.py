97a44bd21a3fa92d56d97afb8b4f1a27202a290d
Polina <vanillapupa@gmail.com>
2017-11-18 05:10:35 +0300

    Добавлена проверка списков в тест для редактирования контакта

--- test/test_update_user.py
+++ test/test_update_user.py
@@ -8,5 +8,11 @@ def test_update_first_user(app):
                        email2="Holmes@Museum.com", hometel="3213213", mobiletel="+441712223355")
         app.user.add(contact)
         app.user.open_home_page()
+    old_user_list = app.user.get_user_list()
     contact = User(firstname="John H.", lastname="Watson", email="JohnH.Watson@Museum.com", email2="Dr.Watson@Museum.com")
+    contact.id = old_user_list[0].id
     app.user.update_first(contact)
+    new_user_list = app.user.get_user_list()
+    assert len(old_user_list) == len(new_user_list)
+    old_user_list[0] = contact
+    assert sorted(old_user_list, key=User.id_or_max) == sorted(new_user_list, key=User.id_or_max)
