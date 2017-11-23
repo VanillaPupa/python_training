0744dc872331c63358347d14f4a70f02422261dd
Polina-PC\Polina <funky.mandarin@gmail.com>
2017-11-23 17:44:25 +0300

    Исправлена ошибка в тесте с удалением контакта

--- test/test_update_user.py
+++ test/test_update_user.py
@@ -4,10 +4,10 @@ from random import randrange
 
 def test_update_some_user(app):
     if app.user.count() == 0:
-        contact = User(firstname="Sherlock", lastname="Holmes",
+        new_contact = User(firstname="Sherlock", lastname="Holmes",
                        address="221b, Baker Street, London, UK", email="Sherlock@Museum.com",
                        email2="Holmes@Museum.com", hometel="3213213", mobiletel="+441712223355")
-        app.user.add(contact)
+        app.user.add(new_contact)
         app.user.open_home_page()
     old_user_list = app.user.get_user_list()
     index = randrange(len(old_user_list))
