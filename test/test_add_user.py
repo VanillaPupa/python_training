67773ae6cbb220bb494e32148a59a421ce57ecd7
Polina <funky.mandarin@gmail.com>
2017-12-07 02:18:56 +0300

    Параметризован тест для добавления контакта

--- test/test_add_user.py
+++ test/test_add_user.py
@@ -1,14 +1,30 @@
 from model.user import User
+import pytest
+import random
+import string
 
 
-def test_add_user(app):
-    # формирование старого списка пользователей
+def random_str(prefix, maxlen):
+    symbols = string.ascii_letters + string.digits + " "*10
+    return prefix + ": " + "".join([random.choice(symbols) for i in range(random.randrange(maxlen))])
+
+
+def random_phone(maxlen):
+    symbols = string.digits + "(" + ")" + "-" + "+" + " "
+    return "".join([random.choice(symbols) for i in range(random.randrange(maxlen))])
+
+
+testdata = [User(firstname="", lastname="", address="", email="", email2="", email3="", homephone="", mobilephone="",
+                 workphone="", additionalphone="")] + \
+           [User(firstname=random_str("firstname", 10), lastname=random_str("lastname", 20),
+                 address=random_str("address", 70), email=random_str("email", 20), email2=random_str("email2", 20),
+                 email3=random_str("email3", 20), homephone=random_phone(10), mobilephone=random_phone(10),
+                 workphone=random_phone(10), additionalphone=random_phone(10))for i in range(5)]
+
+
+@pytest.mark.parametrize("contact", testdata, ids=[repr(x) for x in testdata])
+def test_add_user(app, contact):
     old_user_list = app.user.get_user_list()
-    # данные для нового контакта
-    contact = User(firstname="Sherlock", lastname="Holmes",
-                   address="221b, Baker Street, London, UK", email="Sherlock@museum.com",
-                   email2="Holmes@museum.com", email3="HolmesWatson@museum.com", homephone="321-32-13",
-                   mobilephone="+441712223355", workphone="123 12 31", additionalphone="+(44)1715553322")
     # добавление контакта
     app.user.add(contact)
     # формирование нового списка пользователей
