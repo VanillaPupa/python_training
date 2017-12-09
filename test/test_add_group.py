0c82bc9c96cf162e3bda0cfa09217f3ce9a021c6
Polina <funky.mandarin@gmail.com>
2017-12-10 01:15:15 +0300

    Создан генератор случайных данных в формате JSON для групп

--- test/test_add_group.py
+++ test/test_add_group.py
@@ -1,17 +1,6 @@
 from model.group import Group
+from data.add_group import constant as testdata
 import pytest
-import random
-import string
-
-
-def random_str(prefix, maxlen):
-    symbols = string.ascii_letters + string.digits + string.punctuation + " "*10
-    return prefix + ": " + "".join([random.choice(symbols) for i in range(random.randrange(maxlen))])
-
-
-testdata = [Group(name="", header="", footer="")] + \
-           [Group(name=random_str("name", 10), header=random_str("header", 20),
-                  footer=random_str("footer", 20)) for i in range(5)]
 
 
 @pytest.mark.parametrize("group_form", testdata, ids=[repr(x) for x in testdata])
@@ -22,3 +11,4 @@ def test_create_group(app, group_form):
     assert len(old_groups) + 1 == len(new_groups)
     old_groups.append(group_form)
     assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
+
